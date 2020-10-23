8c06082d753f6a3dc8d3b80ea95a537a22e182a3
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-10-23 15:49:04 +0300

    This is for the discussed topic component

---
+++ components/DiscussedTopics.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+export default function DiscussedTopics({topic}) {
+    const discussedOnDate = new Date(Number(topic.discussedOn));
+    return (
+        <section>
+            <button>Delete</button>
+            <p>{topic.title}</p>
+            <div>
+            <p>Discussed on {discussedOnDate.toLocaleDateString()}</p>
+            </div>
+        </section>
+    )
+}
