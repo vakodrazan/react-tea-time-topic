96aeacec0e0ba6dd34825a2a45e1c7cac4acc978
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-10-24 09:08:09 +0300

    Delete an item from the list when clicking the delete button

--- components/DiscussedTopics.js
+++ components/DiscussedTopics.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import trashIcon from '../assets/trash.svg';
 
-export default function DiscussedTopics({topic}) {
+export default function DiscussedTopics({topic, handleRemove}) {
+
     const discussedOnDate = new Date(Number(topic.discussedOn));
+    // const [topicList, setTopicList] = useState(topic);
+
     return (
         <section>
-            <button className="btn-left">
+            <button 
+                className="btn-left"
+                onClick={() => handleRemove(topic.id)}
+            >
                 <img src={trashIcon} alt="Delete Icon" />
             </button>
             <p>{topic.title}</p>
